3b8ca390be0341d83762fbbc57589845cb122d81
ECCC-CSSP <Charles.LeBlanc2@canada.ca>
2017-08-15 07:24:00 -0300

    Init

---
+++ CSSPDoc/CSSPDocForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CSSPDoc
+{
+    partial class CSSPDocForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxStatus = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBoxStatus
+            // 
+            this.richTextBoxStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBoxStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBoxStatus.Location = new System.Drawing.Point(0, 0);
+            this.richTextBoxStatus.Name = "richTextBoxStatus";
+            this.richTextBoxStatus.Size = new System.Drawing.Size(993, 566);
+            this.richTextBoxStatus.TabIndex = 0;
+            this.richTextBoxStatus.Text = "This Windows form project is only used to generate the help documentation with Do" +
+    "cFX";
+            // 
+            // CSSPDocForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(993, 566);
+            this.Controls.Add(this.richTextBoxStatus);
+            this.Name = "CSSPDocForm";
+            this.Text = "CSSP Documentation Production";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxStatus;
+    }
+}
+
